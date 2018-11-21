e13c08f7738c0d2ec162a88b9955e9f5975281a6
mrvjpman <bob.kong@mail.utoronto.ca>
2018-11-21 11:46:52 -0500

    Starting course 2/5

---
+++ Course 2/1-2-Visual-Studio-Exercise-1-Materials/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleCards;
+
+namespace Exercise1
+{
+    /// <summary>
+    /// Exercise 1 solution
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Practices using arrays
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            Deck deckOfCards = new Deck();
+            Card[] handWithFiveCards = new Card[5];
+            deckOfCards.Shuffle();
+
+            handWithFiveCards[0] = deckOfCards.TakeTopCard();
+            handWithFiveCards[0].FlipOver();
+            handWithFiveCards[0].Print();
+
+            Console.WriteLine();
+
+            handWithFiveCards[1] = deckOfCards.TakeTopCard();
+            handWithFiveCards[1].FlipOver();
+            handWithFiveCards[0].Print();
+            handWithFiveCards[1].Print();
+
+            Console.WriteLine();
+
+        }
+    }
+}
